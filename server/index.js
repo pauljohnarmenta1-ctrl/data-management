c56f6ec9c6e8ba098136c2cedb20f6ceaa859ccf
PAUL <pauljohnarmenta1@gmail.com>
2026-03-10 11:54:38 +0800

    sever

--- server/index.js
+++ server/index.js
@@ -126,6 +126,6 @@ app.post('/api/login', (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.listen(5500, () => {
+  console.log('Server running on http://localhost:5500');
 });
