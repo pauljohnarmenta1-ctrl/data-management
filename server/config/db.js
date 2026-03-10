7ea3997753ed52e427cf94f302779c3c76bd25a0
PAUL <pauljohnarmenta1@gmail.com>
2026-03-10 13:08:37 +0800

    db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,16 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "assessor_system"
+  host: 'localhost',
+  user: 'root',
+  password: 'johnpaul#6101',
+  database: 'assessor_db'
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error("MySQL connection failed:", err);
-    } else {
-        console.log("Connected to MySQL Database");
-    }
+db.connect(err => {
+  if (err) {
+    console.error('MySQL connection failed:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
 });
-
-module.exports = db;
